2648946188b8ab95a8238a8e138ec82153df7356
Bob Tate <btate712@gmail.com>
2020-11-11 15:54:43 -0600

    created randomInt function to generate random numbers between min and max value

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// Declare constants to avoid use of "magic numbers"
+const MIN = 1;
+const MAX = 50;
+
 // Test to verify script.js properly linked to index.html
 main = document.getElementsByTagName("main")[0];
-testLine = document.createElement("div");
+testLine = document.createElement("h1");
 testLine.id = "test";
 testLine.innerHTML = "Hello World!";
 main.appendChild(testLine);
 
-// generate 10 random integers and store those integers in an array
+// test randomInt() in the console
+for(i = 0; i < 10; i++) {
+  console.log(randomInt(MIN, MAX));
+}
 
+// generate 10 random integers and store those integers in an array
+function randomInt(min, max) {
+  return min + Math.floor(Math.random() * Math.floor(max));
+}
 // display the unsorted array and the bubble sort pseudocode
 
 // perform a bubble sort on the array, re-rendering the display
