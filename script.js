40d381f5b1162a0715810d1f4d33b23b5758d9c6
Bob Tate <btate712@gmail.com>
2020-11-11 15:27:19 -0600

    wrote pseudocode as comments in js file

---
+++ script.js
@@ -0,0 +1,7 @@
+// generate 10 random integers and store those integers in an array
+
+// display the unsorted array and the bubble sort pseudocode
+
+// perform a bubble sort on the array, updating the displayed array
+// and explaining the step performed as each line of pseudocode is 
+// performed
