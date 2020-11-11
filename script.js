049032c853e503f60e2d633f59a9ede19c660bdb
Bob Tate <btate712@gmail.com>
2020-11-11 17:41:28 -0600

    Added showArray function to append array of random integers to the DOM

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Declare constants to avoid use of "magic numbers"
 const MIN = 1;
 const MAX = 50;
+const ARRAY_SIZE = 10;
+
+const numbers = [];
 
 // Test to verify script.js properly linked to index.html
 main = document.getElementsByTagName("main")[0];
@@ -9,17 +12,29 @@ testLine.id = "test";
 testLine.innerHTML = "Hello World!";
 main.appendChild(testLine);
 
-// test randomInt() in the console
-for(i = 0; i < 10; i++) {
-  console.log(randomInt(MIN, MAX));
+for(i = 0; i < ARRAY_SIZE; i++) {
+  numbers.push(randomInt(MIN, MAX));
 }
+showArray(numbers);
 
-// generate 10 random integers and store those integers in an array
+// generate random integers and store those integers in an array
 function randomInt(min, max) {
   return min + Math.floor(Math.random() * Math.floor(max));
 }
-// display the unsorted array and the bubble sort pseudocode
 
+// display the unsorted array and the bubble sort pseudocode
+// display the array in the DOM
+function showArray(array) {
+  const arrayHTML = document.createElement("div");
+  arrayHTML.className = "numbers";
+  array.forEach((number, index) => {
+    const line = document.createElement("div");
+    line.id = `array-element-${index}`;
+    line.innerHTML = `${index} - ${number}`;
+    arrayHTML.appendChild(line); 
+  });
+  main.appendChild(arrayHTML);
+}
 // perform a bubble sort on the array, re-rendering the display
 // and explaining the step performed as each line of pseudocode is 
 // performed.
