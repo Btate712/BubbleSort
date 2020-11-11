49f8f1e9b1afc219370a4f6bd0cb1d3ec6bf9e89
Bob Tate <btate712@gmail.com>
2020-11-11 15:28:11 -0600

    saved and re-committed script.js - forgot on last commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 
 // display the unsorted array and the bubble sort pseudocode
 
-// perform a bubble sort on the array, updating the displayed array
+// perform a bubble sort on the array, re-rendering the display
 // and explaining the step performed as each line of pseudocode is 
-// performed
+// performed.
