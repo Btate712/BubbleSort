7a980d8a49ebc10f6757fb57d94c2c4e3a1a0f0a
Bob Tate <btate712@gmail.com>
2020-11-11 18:06:25 -0600

    added bubbleSort function

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 // Declare constants to avoid use of "magic numbers"
 const MIN = 1;
-const MAX = 50;
+const MAX = 100;
 const ARRAY_SIZE = 15;
 
-const numbers = [];
+let numbers = [];
 
 // Get main element from DOM
 main = document.getElementsByTagName("main")[0];
 
+// poulate numbers array with random numbers
 for(i = 0; i < ARRAY_SIZE; i++) {
   numbers.push(randomInt(MIN, MAX));
 }
+// display the unsorted array and the bubble sort pseudocode
+// display the array in the DOM
+console.log(numbers);
+showArray(numbers);
+numbers = bubbleSort(numbers);
 showArray(numbers);
 
-// generate random integers and store those integers in an array
+// Functions:
+
+// bubble sort
+function bubbleSort(numbers) {
+  const array = [...numbers];
+  const n = array.length; 
+
+  for (let i = 0; i < n-1; i++) {
+    for (let j = 0; j < n-i-1; j++) {
+      if (array[j] > array[j+1]) { 
+        // swap array[j+1] and array[j] 
+        console.log(`Swapping elements ${j}: ${array[j]} and ${j + 1}: ${array[j + 1]}`);
+        let temp = array[j]; 
+        array[j] = array[j+1]; 
+        array[j+1] = temp; 
+      } 
+    }
+  }
+  return array;
+}
+
+// generate random integers
 function randomInt(min, max) {
   return min + Math.floor(Math.random() * Math.floor(max));
 }
 
-// display the unsorted array and the bubble sort pseudocode
 // display the array in the DOM
 function showArray(array) {
   const arrayHTML = document.createElement("div");
@@ -29,6 +55,7 @@ function showArray(array) {
     line.innerHTML = `${index} - ${number}`;
     arrayHTML.appendChild(line); 
   });
+  main.innerHTML = "";
   main.appendChild(arrayHTML);
 }
 // perform a bubble sort on the array, re-rendering the display
